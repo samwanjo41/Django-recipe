e8e8393dec959a4f7010e62897179cc1df7667a5
Samuel Wanjohi <swanjohi@tracom.co.ke>
2020-08-25 22:02:21 +0300

    changed links to point to recipe app

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='recipe'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), #class based views
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
