bfc0b4eec604e48b1c15f8da4fb6769c0386d39b
Samuel Wanjohi <swanjohi@tracom.co.ke>
2020-08-27 11:06:38 +0300

    Creates a user profile if new user registers through a signal

--- users/models.py
+++ users/models.py
@@ -4,18 +4,18 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='upload')
+    image = models.ImageField(upload_to='upload', default='default.jpg')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        # img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        # if img.height > 300 or img.width > 300:
+        #     output_size = (300, 300)
+        #     img.thumbnail(output_size)
+        #     img.save(self.image.path)
 
